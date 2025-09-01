6cabd09d0605bf38574fd87a22de5e68c075b7c8
Elaine Soares <elaine.fabiola@gmail.com>
2025-09-01 19:59:36 -0300

    feat: Adiciona classe Cliente

---
+++ .idea/model/Cliente.java
@@ -0,0 +1,29 @@
+package model; //Elaine Fabiola Soares 
+
+public abstract class Cliente {
+    private String documento;
+    private String nome;
+
+    public Cliente(String documento, String nome) {
+        this.documento = documento;
+        this.nome = nome;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public abstract double calcularDesconto(int dias);
+}
