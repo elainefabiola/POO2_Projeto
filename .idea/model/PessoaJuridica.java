fa12fab13dadb350e99af55332475c502a142e80
Elaine Soares <elaine.fabiola@gmail.com>
2025-09-01 20:01:10 -0300

    feat: Adiciona classe PessoaJuridica

---
+++ .idea/model/PessoaJuridica.java
@@ -0,0 +1,24 @@
+package model; //Elaine Fabiola Soares 
+
+public class PessoaJuridica extends Cliente {
+    private String cnpj;
+
+    public PessoaJuridica(String cnpj, String nome) {
+        super(cnpj, nome);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    @Override
+    public double calcularDesconto(int dias) {
+        // RN7: Caso o cliente pessoa jurídica tenha ficado com o carro mais que 3 diárias terá direito a 10% de desconto.
+        if (dias > 3) {
+            return 0.10; // 10% de desconto
+        }
+        return 0.0; // Sem desconto
+    }
+}
+
