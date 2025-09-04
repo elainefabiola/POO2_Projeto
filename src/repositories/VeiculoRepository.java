f16290bb1874385df2b204218c09ebf4e78c102e
Elaine Soares <elaine.fabiola@gmail.com>
2025-09-04 00:32:52 -0300

    adicionar modificações feitas pelo professor na última aula
    adicionar modificações feitas pelo professor na última aula

--- src/repositories/VeiculoRepository.java
+++ src/repositories/VeiculoRepository.java
@@ -13,26 +13,26 @@ import java.util.stream.Collectors;
  *  - Cadastrar, alterar, buscar por parte do nome
  *  - Alugar/Devolver (usa flag disponivel)
  */
-public class VeiculoRepository extends RepositorioMemoria<Veiculo, String> {
-
-    @Override
-    public String getIdentificador(Veiculo veiculo) {
-        return veiculo.getIdentificador(); // placa
-    }
-
-    // Busca por parte do nome (case-insensitive).
-    public List<Veiculo> buscarPorNomeParcial(String termo) {
-        if (termo == null) termo = "";
-        final String t = termo.toLowerCase();
-        return listarTodos().stream()
-                .filter(v -> v.getNome() != null && v.getNome().toLowerCase().contains(t))
-                .collect(Collectors.toList());
-    }
-
-    // Lista apenas os veículos atualmente disponíveis.
-    public List<Veiculo> buscarDisponiveis() {
-        return listarTodos().stream()
-                .filter(Veiculo::isDisponivel)
-                .collect(Collectors.toList());
-    }
-}
+//public class VeiculoRepository  {
+//
+//    @Override
+//    public String getIdentificador(Veiculo veiculo) {
+//        return veiculo.getIdentificador(); // placa
+//    }
+//
+//    // Busca por parte do nome (case-insensitive).
+//    public List<Veiculo> buscarPorNomeParcial(String termo) {
+//        if (termo == null) termo = "";
+//        final String t = termo.toLowerCase();
+//        return listarTodos().stream()
+//                .filter(v -> v.getNome() != null && v.getNome().toLowerCase().contains(t))
+//                .collect(Collectors.toList());
+//    }
+//
+//    // Lista apenas os veículos atualmente disponíveis.
+//    public List<Veiculo> buscarDisponiveis() {
+//        return listarTodos().stream()
+//                .filter(Veiculo::isDisponivel)
+//                .collect(Collectors.toList());
+//    }
+//}
