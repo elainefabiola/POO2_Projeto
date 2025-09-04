a54f153822d92b55be5425eb9c3e4662c99cdacb
Elaine Soares <elaine.fabiola@gmail.com>
2025-09-04 00:26:42 -0300

    Adicionar modificações feitas pelo professor na última aula
    aplicadas as modificações realizadas pelo professor na última aula

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,34 @@ public class Main {
     public static void main(String[] args) {
         // BASE DE DADOS
         List<Cliente> clientes = new ArrayList<>();
-        List<Veiculo> veiculos = new ArrayList<>();
-        List<Aluguel> alugueis = new ArrayList<>();
 
-        // REPOSITÓRIOS
-        ClienteRepository cr = new ClienteRepository();
-        VeiculoRepository vr = new VeiculoRepository();
-        AluguelRepository ar = new AluguelRepository();
 
-        // SERVICES (regras de negócio)
-        ClienteService cs = new ClienteService(cr);
-        VeiculoService vs = new VeiculoService(vr);
-        AluguelService as = new AluguelService(ar, vr, cr);
+        ClienteRepository cr = new ClienteRepository(clientes); ///salvar na base, deletar, listar
+        ClienteService cs = new ClienteService(cr); //regras de negocio, validacoes, possuipendencia
 
-        // VIEW (interface do usuário)
-        MenuPrincipal mp = new MenuPrincipal(cs, vs, as);
-        mp.start(); // inicia o menu de interação
+        cs.cadastrarCliente(new PessoaFisica("78363786", "Ana"));
+        cs.cadastrarCliente(new PessoaFisica("12345", "joao"));
+
+
+        new MenuPrincipal(cs).start();
+
+//
+//
+//        List<Veiculo> veiculos = new ArrayList<>();
+//        List<Aluguel> alugueis = new ArrayList<>();
+//
+//        // REPOSITÓRIOS
+//        ClienteRepository cr = new ClienteRepository();
+//        VeiculoRepository vr = new VeiculoRepository();
+//        AluguelRepository ar = new AluguelRepository();
+//
+//        // SERVICES (regras de negócio)
+//        ClienteService cs = new ClienteService(cr);
+//        VeiculoService vs = new VeiculoService(vr);
+//        AluguelService as = new AluguelService(ar, vr, cr);
+//
+//        // VIEW (interface do usuário)
+//        MenuPrincipal mp = new MenuPrincipal(cs, vs, as);
+//        mp.start(); // inicia o menu de interação
     }
 }
