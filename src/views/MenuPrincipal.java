85994446b07e77c41826067d5e9ffdd328c8813a
Alex Araujo <alexsaspbr@gmail.com>
2025-09-03 22:02:32 -0300

    sub menu cliente

--- src/views/MenuPrincipal.java
+++ src/views/MenuPrincipal.java
@@ -2,6 +2,9 @@ package views;
 
 import services.ClienteService;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class MenuPrincipal{
     private ClienteService clienteService;
     //private VeiculoService veiculoService;
@@ -14,11 +17,90 @@ public class MenuPrincipal{
 
     public void start(){
 
+        int resp = 0;
+
+        do {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("""
+                    
+                    Selecione a opcao: 
+                    
+                    1 - Clientes
+                    
+                    """);
+
+            int opcao = input.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    new MenuCliente(this.clienteService).init();
+
+
+            }
+
+            System.out.println("Deseja continuar 1 - Sim, 0 - Nao");
+            resp = input.nextInt();
+
+        } while(resp != 0);
+
+    }
+
+}
+
+class MenuCliente {
+
+    private ClienteService clienteService;
+
+    public MenuCliente(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    public void init() {
+
+
+        int resp = 0;
+
+        do {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println(" CLIENTES ");
+            this.listar();
+            System.out.println("""
+                
+                1 - Cadastrar
+                2 - Remover
+                3 - Alterar
+                4 - Voltar
+                
+                """);
+
+            int opcao = input.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    new MenuCliente(this.clienteService).init();
+
+
+            }
+
+            System.out.println("Deseja continuar 1 - Sim, 0 - Nao");
+            resp = input.nextInt();
+
+        } while(resp != 0);
+
+
+
+
+    }
+
+
+    public void listar() {
         clienteService.listarTodos().forEach(cliente -> {
             System.out.println(cliente.getNome());
             System.out.println(cliente.getDocumento());
 
         } );
-
     }
+
 }
